7b82580f47750e018256ab1a78140deaccd10e8d
n.luparev <nikita.luparev@gmail.com>
2023-07-04 14:21:22 +0500

    feature/component-testing

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,5 +1,6 @@
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { render } from './test/utilities';
 import Counter from '.';
 
 test('it should render the component', () => {
@@ -20,9 +21,40 @@ test('it should increment when the "Increment" button is pressed', async () => {
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', async () => {
+  const user = userEvent.setup();
 
-test.todo(
-  'it should reset the count when the "Reset" button is pressed',
-  async () => {},
-);
+  render(<Counter initialCount={2} />);
+
+  const element = screen.getByTestId('current-count');
+
+  expect(element).toHaveTextContent('2');
+
+  const button = screen.getByRole('button', { name: 'Increment' });
+
+  await user.click(button);
+
+  expect(element).toHaveTextContent('3');
+});
+
+test('it should reset the count when the "Reset" button is pressed', async () => {
+  const user = userEvent.setup();
+
+  render(<Counter />);
+
+  const element = screen.getByTestId('current-count');
+
+  expect(element).toHaveTextContent('0');
+
+  const button = screen.getByRole('button', { name: 'Increment' });
+
+  await user.click(button);
+
+  expect(element).toHaveTextContent('1');
+
+  const resetButton = screen.getByRole('button', { name: 'Reset' });
+
+  await user.click(resetButton);
+
+  expect(element).toHaveTextContent('0');
+});
